078db10c350157437d7c479a23f987d763fb8e85
Ivan <izzyumkin@gmail.com>
2019-08-01 14:35:38 +0500

    Мелкие ошибочки

---
+++ AppTableViewLearn/Model/StorageMeneger.swift
@@ -0,0 +1,31 @@
+//
+//  StorageMeneger.swift
+//  AppTableViewLearn
+//
+//  Created by Иван Изюмкин on 25/05/2019.
+//  Copyright © 2019 Иван Изюмкин. All rights reserved.
+//
+
+import RealmSwift
+
+let realm = try! Realm()
+
+class StorageMeneger {
+    
+    static func saveObject(_ place: Order) {
+        
+        try! realm.write {
+            realm.add(place)
+        }
+        
+    }
+    
+    static func deleteObject(_ place: Order) {
+        
+        try! realm.write {
+            realm.delete(place)
+        }
+        
+    }
+    
+}
